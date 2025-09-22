a5d70dd3e1d8db1edb06798a1dec1b234ae0e1c1
lhuith <genejmartens@gmail.com>
2025-09-22 17:52:37 -0600

    Kube DB learning and whatever I forget

---
+++ Kubernetes/scratch/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	fmt.Println(os.Getenv("ENV"))
+	fmt.Println("MiniKube, MiniGopher")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "testin\n")
+	})
+
+	http.HandleFunc("/a", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "a route\n")
+	})
+
+	http.HandleFunc("/b", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "b route\n")
+	})
+
+	http.HandleFunc("/replace", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, fmt.Sprintf("replace == %v\n", os.Getenv("REPLACE")))
+	})
+
+	http.HandleFunc("/env", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, os.Getenv("ENV")+"\n")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
